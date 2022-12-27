f90bba703371e24c3fd069ec3476c3de02b6c2c2
Murilo Oliveira <81969402+muaoliveira@users.noreply.github.com>
2022-12-27 19:55:11 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,33 @@
+
+pokemon = {
+    "Pikachu": {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        "skills": ['Choque do trovão', 'Cabeçada'],
+        "caracteristicas": ['#025', 'Elétrico', 'images/pikachu.png', '#ddc470']
+    },
+    "Charmander": {
+        "status": ['HP: 200', 'Ataque: 300', 'Defesa: 400', 'Velocidade: 320', 'Total: 1.220'],
+        "skills": ['Lança Chamas', 'Ataque Rápido'],
+        "caracteristicas": ['#004', 'Fogo', 'images/charmander.png', '#ED8A8B'],
+    },
+    "Bulbasauro": {
+      "status": ['HP: 320', 'Ataque: 510', 'Defesa: 400', 'Velocidade: 200', 'Total: 1.430'],
+      "skills": ['Folhas Navalha', 'Raio Solar'],
+      "caracteristicas": ['#001', 'Grama', 'images/bulbasaur.png', '#49D0B0'],
+    },
+    "Dragonite": {
+        "status": ['HP: 500', 'Ataque: 600', 'Defesa: 400', 'Velocidade: 400', 'Total: 1.900'],
+        "skills": ['Dança do Dragão', 'Velocidade Extrema'],
+        "caracteristicas": ['#149', 'Dragão', 'images/dragonite.png', '#C29791'],
+    },
+    "Gengar": {
+        "status": ['HP: 100', 'Ataque: 200', 'Defesa: 300', 'Velocidade: 400', 'Total: 1.000'],
+        "skills": ['Bola Sombria', 'Lambida'],
+        "caracteristicas": ['#094', 'Trevas', 'images/gengar.png', '#BA68C8'],
+    },
+    "Gyarados": {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 300', 'Total: 1.700'],
+        "skills": ['Jato'" D'"'Água', 'Hidro Bomba'],
+        "caracteristicas": ['#130', 'Água', 'images/gyarados.png', '#76BEFE'],
+    }
+}
